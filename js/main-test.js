537f2cce93dbcdb92779a54d860b7959087630f6
Volodymyr Melnyk <tvoi.pol@gmail.com>
2023-04-06 16:47:22 +0300

    test conflict commit

--- js/main-test.js
+++ js/main-test.js
@@ -1,6 +1,8 @@
 console.log(1);
 console.log(2);
 
-const b = 7;
+const b = 1;
+
+const c = 10
 
 export const a = 5;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 import {a} from "./main-test"
 
 console.log(a)
+
+
+console.log(11)
